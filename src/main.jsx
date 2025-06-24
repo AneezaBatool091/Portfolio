b5e1639746c6f8f76a78af80906766467615a095
AneezaBatool091 <aneezabatool091@gmail.com>
2025-06-24 15:29:47 +0500

    Fix: render Portfolio directly from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Portfolio from './Portfolio'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Portfolio from './Portfolio';
+import './index.css'; // optional if you're using Tailwind or global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Portfolio />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
